857f5ac8801d1560d01a589390400f6c87dc4bb7
r.kapishev <r.kapishev@samsung.com>
2016-04-22 13:55:43 +0300

    Get rid of "?ig_cache"

--- src/InstagramScraper/Media.php
+++ src/InstagramScraper/Media.php
@@ -23,9 +23,9 @@ class Media
         $this->type = $mediaArray['type'];
         $this->createdTime = $mediaArray['created_time'];
         $this->link = $mediaArray['link'];
-        $this->imageLowResolutionUrl = $mediaArray['images']['low_resolution']['url'];
-        $this->imageThumbnailUrl = $mediaArray['images']['thumbnail']['url'];
-        $this->imageStandardResolutionUrl = $mediaArray['images']['standard_resolution']['url'];
+        $this->imageLowResolutionUrl = self::getCleanImageUrl($mediaArray['images']['low_resolution']['url']);
+        $this->imageThumbnailUrl = self::getCleanImageUrl($mediaArray['images']['thumbnail']['url']);
+        $this->imageStandardResolutionUrl = self::getCleanImageUrl($mediaArray['images']['standard_resolution']['url']);
         $this->imageHighResolutionUrl = str_replace('320x320', '1080x1080', $this->imageLowResolutionUrl);
         $this->caption = $mediaArray['caption']['text'];
 
@@ -34,5 +34,10 @@ class Media
             $this->videoStandardResolutionUrl = $mediaArray['videos']['standard_resolution']['url'];
             $this->videoLowBandwidthUrl = $mediaArray['videos']['low_bandwidth']['url'];
         }
+    }
+
+    private static function getCleanImageUrl($imageUrl)
+    {
+        return strpos($imageUrl, '?ig_cache_key=') ? substr($imageUrl, 0, strpos($imageUrl, '?ig_cache_key=')) : $imageUrl;
     }
 }
