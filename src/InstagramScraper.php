c16efe8fdf53100cafd54704757a6a3ccf0fa9ac
raiym <r.kapishev@gmail.com>
2016-05-03 13:21:03 +0300

    require InstagramException

--- src/InstagramScraper.php
+++ src/InstagramScraper.php
@@ -4,4 +4,4 @@ require_once dirname(__FILE__) . '/InstagramScraper/InstagramDataProvider.php';
 require_once dirname(__FILE__) . '/InstagramScraper/InstagramScraper.php';
 require_once dirname(__FILE__) . '/InstagramScraper/Account.php';
 require_once dirname(__FILE__) . '/InstagramScraper/Media.php';
-require_once dirname(__FILE__) . '/InstagramScraper/Exception.php';
+require_once dirname(__FILE__) . '/InstagramScraper/InstagramException.php';
