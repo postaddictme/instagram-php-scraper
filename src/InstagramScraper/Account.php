6698fff03bb981810fac34f3777cdaec43c84db3
raiym <r.kapishev@gmail.com>
2016-06-02 18:11:25 +0300

    Start docs

--- src/InstagramScraper/Account.php
+++ src/InstagramScraper/Account.php
@@ -4,16 +4,70 @@ namespace InstagramScraper;
 
 class Account
 {
+    /**
+     * User id
+     * @var string
+     */
     public $id;
+
+    /**
+     * Username
+     * @var string
+     */
     public $username;
+
+    /**
+     * Full name
+     * @var string
+     */
     public $fullName;
+
+    /**
+     * Profile picture url
+     * @var string
+     */
     public $profilePicUrl;
+
+    /**
+     * Information filled by user
+     * @var string
+     */
     public $biography;
+
+    /**
+     * Url provided by user in profile
+     * @var string
+     */
     public $externalUrl;
+
+    /**
+     * Number of subscriptions
+     * @var integer
+     */
     public $followsCount;
+
+    /**
+     * Number of followers
+     * @var integer
+     */
     public $followedByCount;
+
+    /**
+     * Number of medias published by user
+     * @var integer
+     */
     public $mediaCount;
+
+    /**
+     * true if account is private
+     * @var boolean
+     */
     public $isPrivate;
+
+    /**
+     * true if verified by Instagram as celebrity
+     * @var boolean
+     */
     public $isVerified;
 
     function __construct()
