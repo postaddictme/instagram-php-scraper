6f566c131b71062f8cfda95e53a9a84b21684470
raiym <r.kapishev@gmail.com>
2016-06-05 22:27:24 +0300

    Include Endpoints class

--- src/InstagramScraper.php
+++ src/InstagramScraper.php
@@ -1,6 +1,7 @@
 <?php
 
 require_once dirname(__FILE__) . '/InstagramScraper/Instagram.php';
+require_once dirname(__FILE__) . '/InstagramScraper/Endpoints.php';
 require_once dirname(__FILE__) . '/InstagramScraper/model/Account.php';
 require_once dirname(__FILE__) . '/InstagramScraper/model/Media.php';
 require_once dirname(__FILE__) . '/InstagramScraper/exception/InstagramException.php';
