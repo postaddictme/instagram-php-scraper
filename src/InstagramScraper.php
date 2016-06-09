24cfef15e02bd6768f0853d5bd89a6fca4c8d707
raiym <r.kapishev@gmail.com>
2016-06-09 11:11:19 +0300

    Include Tag class

--- src/InstagramScraper.php
+++ src/InstagramScraper.php
@@ -4,5 +4,6 @@ require_once dirname(__FILE__) . '/InstagramScraper/Instagram.php';
 require_once dirname(__FILE__) . '/InstagramScraper/Endpoints.php';
 require_once dirname(__FILE__) . '/InstagramScraper/Model/Account.php';
 require_once dirname(__FILE__) . '/InstagramScraper/Model/Media.php';
+require_once dirname(__FILE__) . '/InstagramScraper/Model/Tag.php';
 require_once dirname(__FILE__) . '/InstagramScraper/Exception/InstagramException.php';
 require_once dirname(__FILE__) . '/InstagramScraper/Exception/InstagramNotFoundException.php';
