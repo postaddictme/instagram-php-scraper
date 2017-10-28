3ec6222a129e23550a84d57413067589329fb9a2
raiym <r.kapishev@gmail.com>
2017-10-28 19:28:09 +0900

    Fix example

--- examples/getAccountFollowers.php
+++ examples/getAccountFollowers.php
@@ -7,7 +7,7 @@ sleep(2); // Delay to mimic user
 
 $username = 'kevin';
 $followers = [];
-$account = \InstagramScraper\Instagram::getAccount($username);
+$account = $instagram->getAccount($username);
 sleep(1);
 $followers = $instagram->getFollowers($account->getId(), 1000, 100, true); // Get 1000 followers of 'kevin', 100 a time with random delay between requests
 echo '<pre>' . json_encode($followers, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES) . '</pre>';
