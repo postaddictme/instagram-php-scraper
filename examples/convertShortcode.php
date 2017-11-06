3f8f1669094c83d4d0e21e55c9b0a6d615efd6cc
raiym <r.kapishev@gmail.com>
2017-11-06 21:34:50 +0900

    Update example

--- examples/convertShortcode.php
+++ examples/convertShortcode.php
@@ -1,16 +1,12 @@
 <?php
+require __DIR__ . '/../vendor/autoload.php';
 
 use InstagramScraper\Model\Media;
 
-require __DIR__ . '/../vendor/autoload.php';
 
+echo 'Shortcode: ' . Media::getCodeFromId('1270593720437182847_3') . "\n"; // Shortcode: BGiDkHAgBF_
+echo 'Shortcode: ' . Media::getCodeFromId('1270593720437182847') . "\n"; // Shortcode: BGiDkHAgBF_
 
-echo 'CODE: ' . Media::getCodeFromId('1270593720437182847_3');
-// OR
-echo 'CODE: ' . Media::getCodeFromId('1270593720437182847');
-// CODE: BGiDkHAgBF_
-// So you can do like this: instagram.com/p/BGiDkHAgBF_
+// And you can get link to media: instagram.com/p/BGiDkHAgBF_
 
-
-echo 'Media id: ' . Media::getIdFromCode('BGiDkHAgBF_');
-// Media id: 1270593720437182847
+echo 'Media id: ' . Media::getIdFromCode('BGiDkHAgBF_'); // Media id: 1270593720437182847
