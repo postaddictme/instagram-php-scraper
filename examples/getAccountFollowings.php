16e4ea9d50a825faa1e2d36682ce1c549ae69473
raiym <r.kapishev@gmail.com>
2018-01-24 18:45:12 +0300

    Add account followings example

---
+++ examples/getAccountFollowings.php
@@ -0,0 +1,13 @@
+<?php
+require __DIR__ . '/../vendor/autoload.php';
+
+$instagram = \InstagramScraper\Instagram::withCredentials('username', 'password', 'path/to/cache/folder');
+$instagram->login();
+sleep(2); // Delay to mimic user
+
+$username = 'kevin';
+$followers = [];
+$account = $instagram->getAccount($username);
+sleep(1);
+$followers = $instagram->getFollowing($account->getId(), 1000, 100, true); // Get 1000 followings of 'kevin', 100 a time with random delay between requests
+echo '<pre>' . json_encode($followers, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES) . '</pre>';
