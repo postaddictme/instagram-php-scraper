fa3409676151d51bd33f8b81da57e295663e6fa8
Abyr Valg <valga@abyrga.ru>
2019-05-20 17:50:37 +0300

    Media: Load threaded comments

--- src/InstagramScraper/Model/Comment.php
+++ src/InstagramScraper/Model/Comment.php
@@ -26,6 +26,26 @@ class Comment extends AbstractModel
     protected $owner;
 
     /**
+     * @var static[]
+     */
+    protected $childComments = [];
+
+    /**
+     * @var int
+     */
+    protected $childCommentsCount = 0;
+
+    /**
+     * @var bool
+     */
+    protected $hasMoreChildComments = false;
+
+    /**
+     * @var string
+     */
+    protected $childCommentsNextPage = '';
+
+    /**
      * @var bool
      */
     protected $isLoaded = false;
@@ -63,6 +83,38 @@ class Comment extends AbstractModel
     }
 
     /**
+     * @return static[]
+     */
+    public function getChildComments()
+    {
+        return $this->childComments;
+    }
+
+    /**
+     * @return int
+     */
+    public function getChildCommentsCount()
+    {
+        return $this->childCommentsCount;
+    }
+
+    /**
+     * @return bool
+     */
+    public function hasMoreChildComments()
+    {
+        return $this->hasMoreChildComments;
+    }
+
+    /**
+     * @return string
+     */
+    public function getChildCommentsNextPage()
+    {
+        return $this->childCommentsNextPage;
+    }
+
+    /**
      * @param $value
      * @param $prop
      */
@@ -80,6 +132,22 @@ class Comment extends AbstractModel
                 break;
             case 'owner':
                 $this->owner = Account::create($value);
+                break;
+            case 'edge_threaded_comments':
+                if (isset($value['count'])) {
+                    $this->childCommentsCount = (int) $value['count'];
+                }
+                if (isset($value['edges']) && is_array($value['edges'])) {
+                    foreach ($value['edges'] as $commentData) {
+                        $this->childComments[] = static::create($commentData['node']);
+                    }
+                }
+                if (isset($value['page_info']['has_next_page'])) {
+                    $this->hasMoreChildComments = (bool) $value['page_info']['has_next_page'];
+                }
+                if (isset($value['page_info']['end_cursor'])) {
+                    $this->childCommentsNextPage = (string) $value['page_info']['end_cursor'];
+                }
                 break;
         }
     }
