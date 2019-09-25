f295ec484c6204b7d781a2072df7e7aa7b389339
incatech-ai <andrew.aculana@movent.com>
2019-09-25 11:25:46 +0800

    Update Account.php

--- src/InstagramScraper/Model/Account.php
+++ src/InstagramScraper/Model/Account.php
@@ -468,7 +468,7 @@ class Account extends AbstractModel
                 $this->followedByCount = !empty($array[$prop]['count']) ? (int)$array[$prop]['count'] : 0;
                 break;
             case 'follower_count':
-                $this->followedByCount = empty($array[$prop]['count']) ? $value : 0;
+                $this->followedByCount = empty($array['edge_followed_by']['count']) ? $value : 0;
                 break;
             case 'edge_owner_to_timeline_media':
                 $this->initMedia($array[$prop]);
