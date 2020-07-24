960efa84160983457bac7eb7a02e375bd9d0683c
Nabi <nabikaz@gmail.com>
2020-07-25 03:50:24 +0430

    Fixed #731, Change user-agent
    Signed-off-by: Nabi <nabikaz@gmail.com>

--- src/InstagramScraper/Instagram.php
+++ src/InstagramScraper/Instagram.php
@@ -48,7 +48,7 @@ class Instagram
     private $sessionPassword;
     private $userSession;
     private $rhxGis = null;
-    private $userAgent = 'Mozilla/5.0 (Linux; Android 8.1.0; motorola one Build/OPKS28.63-18-3; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/70.0.3538.80 Mobile Safari/537.36 Instagram 72.0.0.21.98 Android (27/8.1.0; 320dpi; 720x1362; motorola; motorola one; deen_sprout; qcom; pt_BR; 132081645)';
+    private $userAgent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.89 Safari/537.36';
     private $customCookies = null;
 
     /**
