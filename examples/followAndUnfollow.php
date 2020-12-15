37c1e6ceadfe827599849cf40b04629b58c26d39
Alexander Solodov <notdest@gmail.com>
2020-12-16 00:02:46 +0300

    Added follow() and unfollow() functions

---
+++ examples/followAndUnfollow.php
@@ -0,0 +1,17 @@
+<?php
+require __DIR__ . '/../../../../vendor/autoload.php';
+
+use InstagramScraper\Instagram;
+use Phpfastcache\Helper\Psr16Adapter;
+
+$instagram  = Instagram::withCredentials(new \GuzzleHttp\Client(), 'login', 'password', new Psr16Adapter('Files'));
+$instagram->login();
+$instagram->saveSession();
+
+$account    = $instagram->getAccount("username");
+
+$instagram->follow($account->getId());
+
+sleep(10);
+
+$instagram->follow($account->getId());
