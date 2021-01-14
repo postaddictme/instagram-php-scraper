96515b977fd064325d68c9c4cbd0acb19aa1d528
ricdijk <richard@vandijkonline.nl>
2021-01-14 11:11:26 +0100

    changed Exemple getHighights to demonstrate how to get Stories

--- examples/getHighlights.php
+++ examples/getHighlights.php
@@ -3,14 +3,20 @@ use Phpfastcache\Helper\Psr16Adapter;
 
 require __DIR__ . '/../vendor/autoload.php';
 
-$instagram = \InstagramScraper\Instagram::withCredentials(new \GuzzleHttp\Client(), 'username', 'password', new Psr16Adapter('Files'));
+$instagram = \InstagramScraper\Instagram::withCredentials(new \GuzzleHttp\Client(), 'username', 'passwd', new Psr16Adapter('Files'));
 $instagram->login();
 
-$userId = $instagram->getAccount('instagram')->getId();
+//$userId = $instagram->getAccount('instagram')->getId();
+$userId = 556625332;
+echo "<pre>";
 
 $highlights = $instagram->getHighlights($userId);
+$hcount=0;
 foreach ($highlights as $highlight) {
-    echo "Highlight info:\n";
+    $hcount++;
+    echo "\n------------------------------------------------------------------------------------------------------------------------\n";
+    echo "Highlight info ($hcount):\n";
+    echo "<img width=80 src='".$highlight->getImageThumbnailUrl()."'>\n";
     echo "Id: {$highlight->getId()}\n";
     echo "Title: {$highlight->getTitle()}\n";
     echo "Image thumbnail url: {$highlight->getImageThumbnailUrl()}\n";
@@ -21,5 +27,32 @@ foreach ($highlights as $highlight) {
     echo " Id: {$account->getId()}\n";
     echo " Username: {$account->getUsername()}\n";
     echo " Profile pic url: {$account->getProfilePicUrl()}\n";
-    echo "\n";
+
+    echo "------------------------------------------------------------------------------------------------------------------------\n";
+
+    $userStories=$instagram->richd_test($highlight->getId());
+    for ($i=0; $i<count($userStories);$i++)
+    {
+      $stories = $userStories[$i]->getStories();
+      //$owner   = $userStories[$i]->getOwner();
+      //echo "\n===========================================================";
+      //echo "\nUserStorie: " . $i;
+      //echo "\nId:         " . $owner['id'];
+      //echo "\nUserName:   " . $owner['username'];
+
+      //for each stories => get Story
+      for ($j=0; $j<count($stories);$j++)
+      {
+          $story = $stories[$j];
+          echo "\n--------------------------------------------------------";
+          echo "\nStorie:         " . $j;
+          echo "\nId:             " . $story['id'];
+          echo "\nCreation Time:  " . $story['createdTime'];
+          echo "\nType:           " . $story['type'];
+          echo "\n<img height=100 src=\"".$story['imageThumbnailUrl']."\">";
+
+      }
+    }
+
 }
+echo "</pre>";
