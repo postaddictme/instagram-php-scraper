60a52e9c32197e9f6a4ec77084563385b687cb5f
Hukumka <nikita_z@rambler.ru>
2021-02-16 17:56:36 +0300

    update useragent

--- src/InstagramScraper/Instagram.php
+++ src/InstagramScraper/Instagram.php
@@ -54,7 +54,7 @@ class Instagram
     private $sessionPassword;
     private $userSession;
     private $rhxGis = null;
-    private $userAgent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.89 Safari/537.36';
+    private $userAgent = 'Mozilla/5.0 (iPhone; CPU iPhone OS 12_3_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 Instagram 105.0.0.11.118 (iPhone11,8; iOS 12_3_1; en_US; en-US; scale=2.00; 828x1792; 165586599)';
     private $customCookies = null;
 
     /**
