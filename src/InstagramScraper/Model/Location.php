59aafba91be0a9065f60125e059d38943a3c813a
beshoo <beshoo@gmail.com>
2021-10-11 08:28:47 +0300

    Add Get for Search by location name

--- src/InstagramScraper/Model/Location.php
+++ src/InstagramScraper/Model/Location.php
@@ -16,7 +16,16 @@ class Location extends AbstractModel
         'lat'             => 'lat',
         'lng'             => 'lng',
         'profile_pic_url' => 'profilePicUrl',
-        'modified'        => 'modified'
+        'modified'        => 'modified',
+        'pk'              => 'pk',
+        'short_name'      => 'short_name',
+        'external_source'      => 'external_source',
+        'address'      => 'address',
+        'city'      => 'city',
+        'has_viewer_saved'      => 'has_viewer_saved',
+        'lng'      => 'lng',
+        'lat'      => 'lat', 
+        'facebook_places_id'      => 'facebook_places_id',    
     ];
     /**
      * @var
@@ -55,6 +64,44 @@ class Location extends AbstractModel
      * @var
      */
     protected $modified;
+
+    /**
+     * @var
+     */
+    protected $pk;
+
+    /**
+     * @var
+     */
+    protected $short_name;
+
+    /**
+     * @var
+     */
+    protected $facebook_places_id;
+
+
+    /**
+     * @var
+     */
+    protected $external_source;
+
+    /**
+     * @var
+     */
+    protected $address;
+
+    /**
+     * @var
+     */
+    protected $city;
+
+    /**
+     * @var
+     */
+    protected $has_viewer_saved;
+
+
 
     /**
      * @return mixed
@@ -118,4 +165,64 @@ class Location extends AbstractModel
     {
         return $this->modified;
     }
+    
+    /**
+     * @return mixed
+     */
+    public function getPk()
+    {
+        return $this->pk;
+    }
+  
+  
+    /**
+     * @return mixed
+     */
+    public function getShortNname()
+    {
+        return $this->short_name;
+    }  
+    
+    /**
+     * @return mixed
+     */
+    public function getFacebookPlacesId()
+    {
+        return $this->facebook_places_id;
+    }  
+    
+    
+    /**
+     * @return mixed
+     */
+    public function externalSource()
+    {
+        return $this->external_source;
+    } 
+    
+    
+    /**
+     * @return mixed
+     */
+    public function getAddress()
+    {
+        return $this->address;
+    }  
+    
+    /**
+     * @return mixed
+     */
+    public function getCity()
+    {
+        return $this->city;
+    }
+    
+    /**
+     * @return mixed
+     */
+    public function isHasViewerSaved()
+    {
+        return $this->has_viewer_saved;
+    } 
+    
 }
