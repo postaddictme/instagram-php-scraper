1f356a305b6f51a4135b5461b9b289caea02d438
denisdenisi4 <86909129+denisdenisi4@users.noreply.github.com>
2022-01-30 01:03:26 +0500

    Move response body from main exception message to separate field and method. Add string representation of HTTP code. (#1032)
    Co-authored-by: test <test>

--- src/InstagramScraper/Exception/InstagramAgeRestrictedException.php
+++ src/InstagramScraper/Exception/InstagramAgeRestrictedException.php
@@ -4,8 +4,8 @@ namespace InstagramScraper\Exception;
 
 class InstagramAgeRestrictedException extends InstagramException
 {
-    public function __construct($message = "", $code = 403, $previous = null)
+    public function __construct($message = "", $code = 403, $responseBody = "", $previous = null)
     {
-        parent::__construct($message, $code, $previous);
+        parent::__construct($message, $code, $responseBody, $previous);
     }
 }
